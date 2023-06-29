f395487d2740c3569fc1495939edad77fc55f6ef
Владимир Виноградов <vladimirvinogradov@MacBook-Air-Vladimir.local>
2023-06-29 22:00:40 +0300

    five commit

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  css: {
-    loaderOptions: {
-        sass: {
-            additionalData: `@import "@/assets/css/style.scss";`,
-        },
-    },
-  },
-
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
